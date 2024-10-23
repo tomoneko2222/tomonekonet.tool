c7c0f55ece8d28381087571f4c1e84c1485cec14
tomoneko2222 <166003396+tomoneko2222@users.noreply.github.com>
2024-10-23 21:44:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll('.cool-button');
+const urls = [
+    "https://github.com/tomoneko2222/discord-webhookspam",
+    "https://tomoneko2222.github.io/github.io/",
+    "https://github.com/tomoneko2222/Token-Checker/tree/main",
+    "https://97.gigafile.nu/0914-d6f07293c3af0e31bc4e0407bfc56eeb8",
+    "https://github.com/tomoneko2222/youtube-video-downloader/tree/main",
+    "https://raw.githubusercontent.com/tomoneko2222/Dinosour-Game/main/cheat.user.js",
+    "https://github.com/tomoneko2222/discord-bot-nuker",
+    "https://tomoneko2222.github.io/discord-nuker/"
+];
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        window.location.href = urls[index];
+    });
+});
+
+function toggleMark(containerId, markType) {
+    const container = document.getElementById(containerId);
+    const newMark = container.querySelector('.mark.new');
+    const modifiedMark = container.querySelector('.mark.modified');
+
+    if (markType === 'new') {
+        newMark.classList.toggle('hidden');
+        modifiedMark.classList.add('hidden');
+    } else if (markType === 'modified') {
+        modifiedMark.classList.toggle('hidden');
+        newMark.classList.add('hidden');
+    } else {
+        newMark.classList.add('hidden');
+        modifiedMark.classList.add('hidden');
+    }
+}
+
+// 使用例:
+toggleMark('container1', 'new'); // 'NEW'マークを表示/非表示
+toggleMark('container2', 'modified'); // 'Modified'マークを表示/非表示
+toggleMark('container3', ''); // マークを非表示
